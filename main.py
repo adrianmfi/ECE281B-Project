a5504224e990fbdafa4c7ac9743da745b7b977f9
Adrian Meidell Fiorito <adrianmefi@gmail.com>
2017-05-15 14:16:52 -0700

    started some refactoring

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import torch.optim as optim
 from torchvision import datasets, transforms, models
 from torch.autograd import Variable
 
+import math
 import time
 import shutil
 import os
@@ -18,8 +19,8 @@ from torchvision import utils
 parser = argparse.ArgumentParser(description='ECE281 CNN Image classification')
 parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
-parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                    help='input batch size for testing (default: 1000)')
+parser.add_argument('--test-batch-size', type=int, default=100, metavar='N',
+                    help='input batch size for testing (default: 100)')
 parser.add_argument('--epochs', type=int, default=10, metavar='N',
                     help='number of epochs to train (default: 10)')
 parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
@@ -38,6 +39,7 @@ parser.add_argument('--resume', default='', type=str, metavar='PATH',
                     help='path to latest checkpoint (default: none)')
 args = parser.parse_args()
 
+
 #Use CUDA if available
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 torch.manual_seed(args.seed)
@@ -46,18 +48,55 @@ if args.cuda:
 
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 
-def train(epoch,model,optimizer,trainLoader,lr_scheduler, initLr):
-    optimizer = lr_scheduler(optimizer, epoch, init_lr = initLr)
-    model.train()
+#Set up the data loaders, optimizer and net
+trainLoader = torch.utils.data.DataLoader(
+    ImageNet('data', train=True,transform = transforms.Compose([
+        transforms.Scale(32),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.478571, 0.44496, 0.392131],[0.26412, 0.255156, 0.269064])
+        ])), 
+    batch_size=args.batch_size, num_workers = args.workers, shuffle=True, **kwargs)
+valLoader = torch.utils.data.DataLoader(
+    ImageNet('data', train=False, transform = transforms.Compose([
+        transforms.Scale(32),
+        transforms.ToTensor(),
+        transforms.Normalize([0.478571, 0.44496, 0.392131],[0.26412, 0.255156, 0.269064])
+        ])),
+    batch_size=args.batch_size, num_workers = args.workers, shuffle=True, **kwargs)
+
+#Set up the model and optimizer
+model_conv = WideResNet(22, 100,4)
+print(model_conv)
+optimizer = optim.SGD(model_conv.fc.parameters(), lr=args.lr, momentum=args.momentum)
+startEpoch = 1
+bestPrecision = 0
+
+#Resume from checkpoint if specified
+if args.resume:
+        if os.path.isfile(args.resume):
+            print("=> loading checkpoint '{}'".format(args.resume))
+            checkpoint = torch.load(args.resume)
+            startEpoch = checkpoint['epoch']
+            bestPrecision = float(checkpoint['best_precision'])
+            print('Best prediction: ',bestPrecision)
+            model_conv.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            print("=> loaded checkpoint (epoch {})"
+                  .format(checkpoint['epoch']))
+if args.cuda:
+    model_conv.cuda()
+def train(epoch):
+    exp_lr_scheduler(epoch, args.lr)
+    model_conv.train()
     for batchIdx, (data, target) in enumerate(trainLoader):
-        startTime = time.clock()
         #Because of bug in dataset, preprocess again and remove
         target = target.long()
         if args.cuda:
             data, target = data.cuda(), target.cuda()
         data, target = Variable(data), Variable(target)    
         optimizer.zero_grad() 
-        output = model(data)
+        output = model_conv(data)
         loss = F.cross_entropy(output, target)
         loss.backward()
         optimizer.step()
@@ -65,10 +104,9 @@ def train(epoch,model,optimizer,trainLoader,lr_scheduler, initLr):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batchIdx * len(data), len(trainLoader.dataset),
                 100. * batchIdx / len(trainLoader), loss.data[0]))
-            endTime = time.clock()
-            print ('Time used per image: ',(endTime-startTime)/len(data*args.log_interval))
-def validate(epoch,model,optimizer,valLoader):
-    model.eval()
+
+def validate(epoch):
+    model_conv.eval()
     valLoss = 0
     correct = 0
     for idx,(data, target) in enumerate(valLoader):##
@@ -76,7 +114,7 @@ def validate(epoch,model,optimizer,valLoader):
             data, target = data.cuda(), target.cuda()
         target = target.long()
         data, target = Variable(data, volatile=True), Variable(target)
-        output = model(data)
+        output = model_conv(data)
         valLoss += F.cross_entropy(output, target).data[0]
         pred = output.data.max(1)[1] # get the index of the max log-probability
         correct += pred.eq(target.data).cpu().sum()
@@ -89,7 +127,7 @@ def validate(epoch,model,optimizer,valLoader):
 
     return correct/len(valLoader.dataset)
 
-def exp_lr_scheduler(optimizer, epoch, init_lr=0.001, lr_decay_epoch=7):
+def exp_lr_scheduler(epoch, init_lr, lr_decay_epoch=7):
     """Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
     lr = init_lr * (0.1**(epoch // lr_decay_epoch))
 
@@ -99,60 +137,28 @@ def exp_lr_scheduler(optimizer, epoch, init_lr=0.001, lr_decay_epoch=7):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
-    return optimizer
-
-def saveCheckpoint(state, isBest, filename='models/checkpoint.pth.tar'):
+def saveCheckpoint(state, isBest, filename='saved_models/checkpoint.pth.tar'):
     torch.save(state, filename)
     if isBest:
-        shutil.copyfile(filename, 'models/model_best.pth.tar')
+        shutil.copy(filename, 'saved_models/model_best.pth.tar')
 
-if __name__ == '__main__':
-    
-    #Set up the data loaders, optimizer and net
-    trainLoader = torch.utils.data.DataLoader(
-        ImageNet('data', train=True,transform = transforms.Compose([
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])
-            ])), 
-        batch_size=args.batch_size, num_workers = args.workers, shuffle=True, **kwargs)
-    valLoader = torch.utils.data.DataLoader(
-        ImageNet('data', train=False, transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])
-            ])),
-        batch_size=args.batch_size, num_workers = args.workers, shuffle=True, **kwargs)
 
-    #Set up the model and optimizer
-    model_conv = models.resnet18(pretrained=True)
-    for param in model_conv.parameters():
-        param.requires_grad = False
-    num_ftrs = model_conv.fc.in_features
-    model_conv.fc = nn.Linear(num_ftrs, 100)
-    optimizer = optim.SGD(model_conv.fc.parameters(), lr=args.lr, momentum=args.momentum)
-    startEpoch = 1
-    bestPrecision = 0
-    #Resume from checkpoint if specified
-    if args.resume:
-            if os.path.isfile(args.resume):
-                print("=> loading checkpoint '{}'".format(args.resume))
-                checkpoint = torch.load(args.resume)
-                startEpoch = checkpoint['epoch']
-                bestPrecision = checkpoint['best_precision']
-                model.load_state_dict(checkpoint['state_dict'])
-                optimizer.load_state_dict(checkpoint['optimizer'])
-                print("=> loaded checkpoint (epoch {})"
-                      .format(checkpoint['epoch']))
-    if args.cuda:
-        model.cuda()
-    #Train
-    for epoch in range(startEpoch, startEpoch+args.epochs + 1):
-        train(epoch,model_conv,optimizer,trainLoader,exp_lr_scheduler,args.lr)
-        precision = validate(epoch,model_conv,optimizer,valLoader)
-        isBest = precision > bestPrecision
-        saveCheckpoint({
-            'epoch': epoch + 1,
-            'state_dict': model_conv.state_dict(),
-            'best_precision': bestPrecision,
-            'optimizer' : optimizer.state_dict(),
-        }, isBest)
+#Train
+for epoch in range(startEpoch, startEpoch+args.epochs + 1):
+    startTime = time.clock()
+    train(epoch)
+    endTime = time.clock()
+    print ('Time used per epoch: ',(endTime-startTime))
+    precision = validate(epoch)
+    isBest = False
+    if precision > bestPrecision:
+        bestPrecision = precision
+        isBest = True 
+    saveCheckpoint({
+        'epoch': epoch + 1,
+        'state_dict': model_conv.state_dict(),
+        'best_precision': bestPrecision,
+        'optimizer' : optimizer.state_dict(),
+    }, isBest)
+
+
